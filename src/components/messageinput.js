006bacba730f2bafa9190b5785d62a7ab02f3669
Iago Elias <iago@sflabs.com.br>
2020-05-19 18:53:24 -0300

    added material icons and core

--- src/components/messageinput.js
+++ src/components/messageinput.js
@@ -1,7 +1,6 @@
 import React from "react"
 import styles from "../styles/messageinput.module.scss"
 import logo from "../images/interface.png"
-
 class Input extends React.Component {
 
     constructor (props) {
