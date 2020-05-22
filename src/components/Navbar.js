c56f15995cde9ce87d0e31232d05f52a995150cd
Pedro Brando <pedro.brando@cursoenfase.com.br>
2020-05-22 16:37:24 -0300

    New Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,10 +3,20 @@ import HomeIcon from "@material-ui/icons/Home"
 import IconButton from "@material-ui/core/IconButton"
 import InfoIcon from "@material-ui/icons/Info"
 import React from "react"
+import { navigate } from "gatsby"
 import styled from "styled-components"
 
 const Container = styled.div`
-  height: 100%;
+  && {
+    position: fixed;
+    bottom: 0;
+    height: 50px;
+    width: 100%;
+  }
+`
+
+const Bar = styled.div`
+  height: 50px;
   display: flex;
   justify-content: center;
   align-items: flex-end;
@@ -14,7 +24,7 @@ const Container = styled.div`
 
 const NavbarLayout = styled.div`
   && {
-    height: 60px;
+    height: 40px;
   }
   display: flex;
   justify-content: center;
@@ -34,33 +44,28 @@ const StyledIconButton = styled(IconButton)`
   }
 `
 
-const IconLabel = styled.span`
-  font-size: 12px;
-`
-
 const Navbar = () => {
   return (
     <Container>
-      <NavbarLayout>
-        <IconBox>
-          <StyledIconButton>
-            <InfoIcon fontSize="large" />
-          </StyledIconButton>
-          <IconLabel>Info</IconLabel>
-        </IconBox>
-        <IconBox>
-          <StyledIconButton>
-            <HomeIcon fontSize="large" />
-          </StyledIconButton>
-          <IconLabel>Inicio</IconLabel>
-        </IconBox>
-        <IconBox>
-          <StyledIconButton>
-            <ContactSupportIcon fontSize="large" />
-          </StyledIconButton>
-          <IconLabel>Ajuda</IconLabel>
-        </IconBox>
-      </NavbarLayout>
+      <Bar>
+        <NavbarLayout>
+          <IconBox>
+            <StyledIconButton onClick={() => navigate("/info")}>
+              <InfoIcon fontSize="large" />
+            </StyledIconButton>
+          </IconBox>
+          <IconBox>
+            <StyledIconButton onClick={() => navigate("/home")}>
+              <HomeIcon fontSize="large" />
+            </StyledIconButton>
+          </IconBox>
+          <IconBox>
+            <StyledIconButton onClick={() => navigate("/contact")}>
+              <ContactSupportIcon fontSize="large" />
+            </StyledIconButton>
+          </IconBox>
+        </NavbarLayout>
+      </Bar>
     </Container>
   )
 }
