4c70e76b8989823208c20be37eac5c2daff44139
Pedro Brando <pedro.brando@cursoenfase.com.br>
2020-06-30 00:48:56 -0300

    Admin Pages front mock done

--- src/pages/AddingAtendent.js
+++ src/pages/AddingAtendent.js
@@ -5,36 +5,60 @@ import DialogContent from "@material-ui/core/DialogContent"
 import DialogContentText from "@material-ui/core/DialogContentText"
 import DialogTitle from "@material-ui/core/DialogTitle"
 import ImageWithDescription from "../components/FormSelection/ImageWithDescription"
-import InputField from "../components/Login/InputField"
-import MenuItem from "@material-ui/core/MenuItem"
-import TextField from "@material-ui/core/TextField"
+import { navigate } from "gatsby"
 import styles from "../styles/AddingAtendent.module.scss"
 import terapia from "../images/terapia.jpg"
 
 const AddingAtendents = () => {
+  const [nome, setNome] = useState("")
+  const [email, setEmail] = useState("")
   const [instituto, setInstituto] = useState("")
+  const [link, setLink] = useState("")
   const [calendarLinkDialogOpen, setCalendarLinkDialogOpen] = useState(false)
+  const [errorDialogOpen, setErrorDialogOpen] = useState(false)
 
-  const institutos = ["ICMC", "EESQ", "IFSC", "IQSC"]
+  const institutos = ["-", "ICMC", "EESQ", "IFSC", "IQSC"]
 
   const options = institutos.map(instituto => {
     return <option value={instituto}>{instituto}</option>
   })
 
-  const handleChange = event => {
-    setInstituto(event.target.value)
-  }
-
   const handleAddingClick = () => {
-    setCalendarLinkDialogOpen(!calendarLinkDialogOpen)
+    console.log(nome, email, link, instituto)
+    //se der certo
+    setCalendarLinkDialogOpen(true)
+    //se der errado
+    //setErrorDialogOpen(true)
   }
 
-  const handleDialog = () => {
-    setCalendarLinkDialogOpen(!calendarLinkDialogOpen)
+  const handleCloseDialog = () => {
+    setCalendarLinkDialogOpen(false)
+    navigate("/Atendents")
+  }
+
+  const handleCloseErrorDialog = () => {
+    setErrorDialogOpen(false)
   }
 
   return (
     <>
+      <Dialog open={errorDialogOpen}>
+        <DialogTitle>Instituto Inválido</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Só é possível adicionar um único atendente por instituto.
+          </DialogContentText>
+        </DialogContent>
+        <div className={styles.ButtonBox}>
+          <button
+            className={styles.Button}
+            onClick={() => handleCloseErrorDialog()}
+          >
+            Fechar
+          </button>
+        </div>
+      </Dialog>
+
       <Dialog open={calendarLinkDialogOpen}>
         <DialogTitle>Link Google Agenda</DialogTitle>
         <DialogContent>
@@ -45,58 +69,59 @@ const AddingAtendents = () => {
           <a className={styles.LinkText}>www.google.br</a>
         </DialogContent>
         <div className={styles.ButtonBox}>
-          <button className={styles.Button} onClick={() => handleDialog()}>
+          <button className={styles.Button} onClick={() => handleCloseDialog()}>
             Fechar
           </button>
         </div>
       </Dialog>
       <ImageWithDescription src={terapia} title={"Adicionar Atendente"} />
-      <form>
-        <div className={styles.Form}>
-          <div className={styles.InputBox}>
-            <span className={styles.InputLabel}>Nome</span>
-            <input
-              className={styles.InputField}
-              placeholder="Nome"
-              type="text"
-            />
-          </div>
-          <div className={styles.InputBox}>
-            <span className={styles.InputLabel}>Email</span>
-            <input
-              className={styles.InputField}
-              placeholder="Email"
-              type="email"
-            />
-          </div>
-          <div className={styles.InputBox}>
-            <span className={styles.InputLabel}>Link da Agenda</span>
-            <input
-              className={styles.InputField}
-              placeholder="Link"
-              type="text"
-            />
-          </div>
-          <div className={styles.InputBox}>
-            <span className={styles.InputLabel}>Instituto</span>
-            <select className={styles.InputField} placeholder="Instituto">
-              {options}
-            </select>
-          </div>
+      <div className={styles.Form}>
+        <div className={styles.InputBox}>
+          <span className={styles.InputLabel}>Nome</span>
+          <input
+            className={styles.InputField}
+            placeholder="Nome"
+            type="text"
+            onChange={e => setNome(e.target.value)}
+          />
         </div>
-        <div className={styles.ButtonBox}>
-          <button
-            className={styles.AddButton}
-            onClick={handleAddingClick}
-            type="submit"
+        <div className={styles.InputBox}>
+          <span className={styles.InputLabel}>Email</span>
+          <input
+            className={styles.InputField}
+            placeholder="Email"
+            type="email"
+            onChange={e => setEmail(e.target.value)}
+          />
+        </div>
+        <div className={styles.InputBox}>
+          <span className={styles.InputLabel}>Link da Agenda</span>
+          <input
+            className={styles.InputField}
+            placeholder="Link"
+            type="text"
+            onChange={e => setLink(e.target.value)}
+          />
+        </div>
+        <div className={styles.InputBox}>
+          <span className={styles.InputLabel}>Instituto</span>
+          <select
+            className={styles.InputField}
+            placeholder="Instituto"
+            onChange={e => setInstituto(e.target.value)}
           >
-            ADICIONAR
-          </button>
-          <a className={styles.Link} href={"/Atendents"}>
-            <button className={styles.CancelButton}>CANCELAR</button>
-          </a>
+            {options}
+          </select>
         </div>
-      </form>
+      </div>
+      <div className={styles.ButtonBox}>
+        <button className={styles.AddButton} onClick={handleAddingClick}>
+          ADICIONAR
+        </button>
+        <a className={styles.Link} href={"/Atendents"}>
+          <button className={styles.CancelButton}>CANCELAR</button>
+        </a>
+      </div>
     </>
   )
 }
--- src/pages/ConfirmingAtendent.js
+++ src/pages/ConfirmingAtendent.js
@@ -8,22 +8,58 @@ import ImageWithDescription from "../components/FormSelection/ImageWithDescripti
 import InputField from "../components/Login/InputField"
 import MenuItem from "@material-ui/core/MenuItem"
 import TextField from "@material-ui/core/TextField"
+import { navigate } from "gatsby"
 import styles from "../styles/ConfirmingAtendent.module.scss"
 import terapia from "../images/terapia.jpg"
 
 const ConfirmingAtendent = () => {
+  const [codigo, setCodigo] = useState("")
   const [calendarLinkDialogOpen, setCalendarLinkDialogOpen] = useState(false)
+  const [errorDialogOpen, setErrorDialogOpen] = useState(false)
 
-  const handleGenerateLinkClick = () => {
-    setCalendarLinkDialogOpen(!calendarLinkDialogOpen)
+  const handleSendClick = () => {
+    console.log(codigo)
+    //Se tiver sucesso
+    navigate("/Atendents")
+    //Se estiver errado
+    //setErrorDialogOpen(true)
   }
 
-  const handleDialog = () => {
-    setCalendarLinkDialogOpen(!calendarLinkDialogOpen)
+  const handleGenerateLinkClick = () => {
+    //precisa ter acesso ao link da agenda cadastrado, nn sei como passar isso entre paginas do gatsby
+    setCalendarLinkDialogOpen(true)
+  }
+
+  const handleCancelClick = () => {
+    navigate("/Atendents")
+  }
+
+  const handleCloseDialog = () => {
+    setCalendarLinkDialogOpen(false)
+  }
+
+  const handleCloseErrorDialog = () => {
+    setErrorDialogOpen(false)
   }
 
   return (
     <>
+      <Dialog open={errorDialogOpen}>
+        <DialogTitle>Código incorreto</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            O codigo inserido não está correto, por favor tente novamente
+          </DialogContentText>
+        </DialogContent>
+        <div className={styles.DialogButtonBox}>
+          <button
+            className={styles.Button}
+            onClick={() => handleCloseErrorDialog()}
+          >
+            Fechar
+          </button>
+        </div>
+      </Dialog>
       <Dialog open={calendarLinkDialogOpen}>
         <DialogTitle>Link Google Agenda</DialogTitle>
         <DialogContent>
@@ -34,41 +70,42 @@ const ConfirmingAtendent = () => {
           <a className={styles.LinkText}>www.google.br</a>
         </DialogContent>
         <div className={styles.DialogButtonBox}>
-          <button className={styles.Button} onClick={() => handleDialog()}>
+          <button className={styles.Button} onClick={() => handleCloseDialog()}>
             Fechar
           </button>
         </div>
       </Dialog>
       <ImageWithDescription src={terapia} title={"Confirmar Atendente"} />
-      <form>
-        <div className={styles.Form}>
-          <div className={styles.InputBox}>
-            <span className={styles.InputLabel}>Código do Google Agenda</span>
-            <input
-              className={styles.InputField}
-              placeholder="Código"
-              type="text"
-            />
-          </div>
+      <div className={styles.Form}>
+        <div className={styles.InputBox}>
+          <span className={styles.InputLabel}>Código do Google Agenda</span>
+          <input
+            className={styles.InputField}
+            placeholder="Código"
+            type="text"
+            onChange={e => setCodigo(e.target.value)}
+          />
         </div>
+      </div>
 
-        <div className={styles.ButtonBox}>
-          <button className={styles.AddButton} type="submit">
-            ENVIAR
+      <div className={styles.ButtonBox}>
+        <button className={styles.AddButton} onClick={() => handleSendClick()}>
+          ENVIAR
+        </button>
+        <a className={styles.Link} href={"/Atendents"}>
+          <button className={styles.CancelButton} onClick={handleCancelClick}>
+            CANCELAR
           </button>
-          <a className={styles.Link} href={"/Atendents"}>
-            <button className={styles.CancelButton}>CANCELAR</button>
-          </a>
-        </div>
-        <div className={styles.CalendarButtonBox}>
-          <button
-            className={styles.CalendarButton}
-            onClick={handleGenerateLinkClick}
-          >
-            GERAR LINK DE CONFIRMAÇÃO
-          </button>
-        </div>
-      </form>
+        </a>
+      </div>
+      <div className={styles.CalendarButtonBox}>
+        <button
+          className={styles.CalendarButton}
+          onClick={handleGenerateLinkClick}
+        >
+          GERAR LINK DE CONFIRMAÇÃO
+        </button>
+      </div>
     </>
   )
 }
